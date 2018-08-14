8e715408ef2ecb2fb2500068cc5d4ec489f3aa2d
elias_ubuntu <eliasr0595@gmail.com>
2018-08-13 21:31:51 -0500

    adding function to scrapp Porrua's web page in scrapping_books.py

--- scrapping_books.py
+++ scrapping_books.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/scrappingGandhi/<searchPhrase>")
-def scrapping_books(searchPhrase):
+def scrapping_booksGandhi(searchPhrase):
     modifed_phrase = searchPhrase.replace(" ","+")
     quote_page = "https://busqueda.gandhi.com.mx/busca?q="+modifed_phrase
     page = urllib2.urlopen(quote_page)
@@ -22,6 +22,7 @@ def scrapping_books(searchPhrase):
     	find_price = booksItems.find('p',class_="special-price") 
     	find_price2 = find_price.find('span',class_="price") 
     	find_format = booksItems.find('h4').text.strip()
+        book_writer =  booksItems.find('h3').text.strip()
 
         if find_price2 != None:
         	book_price = find_price2.text
@@ -30,14 +31,52 @@ def scrapping_books(searchPhrase):
 
         book["cover"] = book_image
         book["tile"] = book_title
+        book["writer"] = book_writer
         book["price"] = book_price
-
-        if find_format:
-        	book["format"] = find_format
+        book["format"] = find_format
 
 
         books.append(book)
         
 
     return json.dumps(books)
+
+
+
+@app.route("/scrappingPorrua/<searchPhrase>")
+def scrapping_booksPorrua(searchPhrase):
+    modifed_phrase = searchPhrase.replace(" ","%20")
+    quote_page = "https://www.porrua.mx/busqueda/todos/"+modifed_phrase
+    page = urllib2.urlopen(quote_page)
+
+    books = []
+    soup = BeautifulSoup(page.read(),'html.parser')
+    books_search = soup.find(id="librosContainer")
+    
+    for booksItems in books_search.find_all('div',class_="col-md-3 col-sm-4"):
+        book = {}
+        find_image = booksItems.a
+        book_image = find_image.img['src']
+        book_title = booksItems.h5.contents[0]
+        book_writer = booksItems.h5.font.text
+        book_price = booksItems.h5.strong.text.strip()
+        find_format = booksItems.find(class_="imgs_ebook")
+
+
+        if find_format != None:
+            find_format = ("ebook")
+        else:
+            find_format = ("fisico") 
+
+        book["cover"] = book_image
+        book["title"] = book_title
+        book["writer"] = book_writer
+        book["price"] = book_price
+        book["format"] = find_format
+
+
+        
+        books.append(book)
+
+    return json.dumps(books)  
 
